ba55bd7d4935a8fe72fba38a9de6855d1a2b477e
farhahdin1997 <farhahdin97@hotmail.com>
2022-05-05 20:58:46 +0100

    function questions added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Employee = require('./employee');
 require ("dotenv").config ();
 
 
-
+/*Connecting to database sql */
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -25,4 +25,56 @@ const connection = mysql.createConnection({
     database: 'myCompany_db'
 }); 
 
+/* Displays the welcome message but if the connection fails display error else go to the next part and run that function ini|(0*/
+console.log('\n\nWelcome to the Employee Tracker\n\n===============================');
+connection.connect(function (err){
+    if (err)throw err 
+});
+questions();
 
+function questions() {
+    console.log('\n\n')
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'init',
+            message: 'What would you like to do?',
+            choices: ['View Departments', 'View Roles', 'View Employees', 'View Department Budget', 'Update Employee','Add Department', 'Add Role', 'Add Employee', 'Delete Department', 'Delete Role', 'Delete Employee', 'Exit Employee Tracker'],
+            pageSize: 12
+        }
+    ]).then((answers) => {
+        switch(answers.init) {
+            case 'Exit Employee Tracker':
+                connection.end();
+                console.log('Goodbye');
+                /* Once exited will display goodbye*/
+                break;
+                /* Will run the fucntions to allow them to work*/
+            case 'Update Employee':
+                updateEmployee();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'View Departments':
+                viewDepartments();
+                break;
+            case 'View Employees':
+                viewEmployees();
+                break;
+            case 'View Roles':
+                viewRoles();
+                break;
+            case 'View Department Budget':
+                viewDepartmentBudget();
+                break;
+            
+        }
+    })
+}
