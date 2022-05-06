c173ba38675c29a1f2b68d9a336cd1b97a56a5e8
farhahdin1997 <farhahdin97@hotmail.com>
2022-05-06 10:34:39 +0100

    changed seeds

--- db/seed.sql
+++ db/seed.sql
@@ -1,18 +1,19 @@
-USE employer_trackerDB; 
+USE myCompanyEmployeeTracker;
 
 INSERT INTO department (department_name)
-VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal"), ("Administration");
+VALUES ("Sales"), ("IT"), ("Marketing"), ("HR"), ("Administration");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 100000, 2), ("Software Engineer", 70000, 2), 
-("Sales Lead", 500000, 1), ("Salesperson", 150000, 1),
-("Legal Team Lead", 200000, 4), ("Lawyer", 100000, 4),
-("Accountant", 75000, 3), ("CEO", 1000000, 5);
+VALUES 
+("Sales Lead", 30000, 1), ("Salesperson", 150000, 1),
+("Software support", 20000, 2), ("Software Engineer", 70000, 2), 
+("Marketing lead", 25000, 3), ("Marketing associate", 12000, 3),
+("HR Lead", 34000,4), ("Admin Asistant", 14000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 8), ("Mike", "Chan", 2, 1), 
-("Ashley", "Rodriguez", 3, 8), ("Dev", "Patel", 4, 3),
-("Kevin", "Tupik", 5, 8), ("Malia", "Brown", 6, 5),
-("Alexa", "Villanueva", 7, 8), ("Arturo", "Martinez", 8, null);
+VALUES ("Aisha", "Din", 1, 8), ("Kenny", "Chan", 2, 1), 
+("Ashley", "Tidsale", 3, 8), ("Mandeep", "Patel", 4, 3),
+("Kevin", "Hear", 5, 8), ("Rock", "Johson", 6, 5),
+("Bob", "Marley", 7, 8), ("Sadaf", "Din", 8, null);
 
-/*CHANGE BEFORE COMIMIT*/
+ 
